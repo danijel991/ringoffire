30f1b1f3484b7f078810b2604a6eaedfcf170711
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-05-18 21:56:40 +0200

    Show player names from array

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Dany', 'Mila', 'Bjorn'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
